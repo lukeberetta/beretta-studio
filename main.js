733372e1b5ca515c52445e0bf353c3f443df6357
Luke Beretta <thelukeberetta@gmail.com>
2018-06-07 09:47:47 +0200

    added belt

--- main.js
+++ main.js
@@ -6,22 +6,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let minimalist = document.getElementById("btc-minimalist");
   let classic = document.getElementById("btc-classic");
   let keyring = document.getElementById("btc-keyring");
+  let belt = document.getElementById("btc-belt");
 
   let API = new XMLHttpRequest();
 
-  API.onreadystatechange = function() {
+  API.onreadystatechange = function () {
     if (API.readyState == 4 && API.status == 200) {
       var data = JSON.parse(API.responseText);
-      var oneBtc = data.bpi.GBP.rate_float;
+      var btc_gbp = data.bpi.GBP.rate_float;
       // ZAR/GBP is currently hard coded.
-      var onePound = 16.68;
-      var oneMin = ((399/onePound)/oneBtc).toFixed(4);
-      var oneClass = ((499/onePound)/oneBtc).toFixed(4);
-      var oneKey = ((99/onePound)/oneBtc).toFixed(4);
+      var gbp = 17.1;
+      var minimalistBtcPrice = ((399 / gbp) / btc_gbp).toFixed(4);
+      var classicBtcPrice = ((499 / gbp) / btc_gbp).toFixed(4);
+      var keyringBtcPrice = ((99 / gbp) / btc_gbp).toFixed(4);
+      var beltBtcPrice = ((599 / gbp) / btc_gbp).toFixed(4);
       // Place into DOM
-      minimalist.innerText = oneMin;
-      classic.innerText = oneClass;
-      keyring.innerText = oneKey;
+      minimalist.innerText = minimalistBtcPrice;
+      classic.innerText = classicBtcPrice;
+      keyring.innerText = keyringBtcPrice;
+      belt.innerText = beltBtcPrice;
     }
   }
 
